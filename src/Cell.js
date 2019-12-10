3e9857f888abd983a5ac61779c804279785c699f
saja amer <sajaamer86@gmail.com>
2019-12-10 05:42:42 +0000

    solved

--- src/Cell.js
+++ src/Cell.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 
 export default class Cell extends Component {
-    constructor(props) {
-            super(props)
-            this.state = {color:this.props.value} 
-    }  
-    change=()=>{
-            this.setState({
-                color:'#333' 
-            })
-    } 
-    render(){
-               return <div className="cell" style={{backgroundColor: this.state.color}} onClick={this.change}>hi</div>
-
-       
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      color: this.props.value
     }
+  }
+  
+  handleClick = () => {
+    this.setState({
+      color: '#333'
+    })
+  }
+  
+  render() {
+    return (
+      <div 
+        className="cell"
+        style={{backgroundColor: this.state.color}}
+        onClick={this.handleClick}>
+      </div>
+    )
+  }
+  
 }
--- src/Matrix.js
+++ src/Matrix.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 import Cell from './Cell.js'
+
 export default class Matrix extends Component {
   
   genRow = (vals) => (
-   // console.log(vals)
-     vals.map(val =><Cell value={val} />) // replace me and render a cell component instead!
+    vals.map(val => <Cell value={val} />)
   )
   
   genMatrix = () => (
-   //console.log(this.props.values),
     this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
-  
   )
   
   render() {
@@ -21,20 +19,11 @@ export default class Matrix extends Component {
     )
   }
   
- 
 }
 
-Matrix.defaultProps={
-  values :[
-    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
-    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
-    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
-    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
-    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
-    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
-    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
-    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
-    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
-    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
-  ]
-  } 
+Matrix.defaultProps = {
+  values: (() => {
+    const defRow = ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+    return (new Array(10).fill(defRow))
+  })()
+}
